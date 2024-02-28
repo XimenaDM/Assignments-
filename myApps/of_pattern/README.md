1e9a05beb87d52bdfb1a2d549aefb8e744b1b666
XimenaDM <156861388+XimenaDM@users.noreply.github.com>
2024-02-28 18:40:46 -0500

    Add files via upload

---
+++ myApps/of_pattern/README.md
@@ -0,0 +1,2 @@
+This must be placed three levels down in your openFrameworks folder to compile:
+./apps/myApps/of_pattern
---
+++ myApps/of_pattern/src/ofApp.cpp
@@ -0,0 +1,83 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0);
+
+    int step = 10;
+    
+    for (int i=0; i<ofGetWidth(); i += step) {
+        for (int j=0; j<ofGetHeight(); j += step) {
+            ofSetColor(0, 127, 0, 20);
+            ofDrawCircle(i, j, step*2, step*2);
+
+            ofSetColor(227, 100, 0, 127);
+            ofDrawCircle(i, j, step, step);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
